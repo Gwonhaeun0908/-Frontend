398dde16f5e2a4873e3b7b6635278ff7b0257fba
eun <haeun0908@dsm.hs.kr>
2022-07-07 23:43:53 +0900

    To do list js

---
+++ To do list.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const todolist = document.getElementById("todolist");
+
+button.addEventListener('click', function()) {
+    let list = document.createElement('li');
+    if(!input.value)
+        alert('내용을 입력해 주세요!');
+    else
+    {
+        list.
+    }
+}
+
