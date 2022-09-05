5fe7de3362d6fb0f89eee1cff4f592a83cffa660
eun <haeun0908@dsm.hs.kr>
2022-09-05 23:44:52 +0900

    📝 :: NewTimes Update

--- Project/NewsTimes/NewsTimes.js
+++ Project/NewsTimes/NewsTimes.js
@@ -1,4 +1,6 @@
 let news = [];
+let menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) => menu.addEventListener("click",(event) => getNewsByTopic(event)));
 
 const getLatesNews = async () => {
     let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
@@ -12,20 +14,24 @@ const getLatesNews = async () => {
     render()
 };
 
+const getNewsByTopic = (event) => {
+
+}
+
 const render = () => {
     let newsHTML = ''   ;
-    newsHTML = news.map((news) => {
+    newsHTML = news.map((item) => {
         return `<div class="row news">
         <div class="col-lg-4">
-            <img class="news-img-size" src="${news.media}"/>
+            <img class="news-img-size" src="${item.media}"/>
         </div>
         <div class="col-lg-8">
-            <h2>${news.title}</h2>
+            <h2>${item.title}</h2>
             <p>
-                ${news.summary}
+                ${item.summary}
             </p>
             <div>
-                ${news.rights} ${news.published_date}
+                ${item.rights} ${item.published_date}
             </div>
         </div>
     </div>`;
@@ -35,3 +41,20 @@ const render = () => {
 };
 
 getLatesNews();
+
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if(inputArea.style.display === "inline") {
+        inputArea.style.display = "none";
+    } else {
+        inputArea.style.display = "inline";
+    }
+};
+
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
