ebf61efa6923c33b49db9f38e532d3a031759f0d
eun <haeun0908@dsm.hs.kr>
2022-09-13 23:29:47 +0900

    ✨ :: NewsTimes 검색기능과 NoImg 사진 추가

--- Project/NewsTimes/NewsTimes.js
+++ Project/NewsTimes/NewsTimes.js
@@ -1,29 +1,65 @@
 let news = [];
 let menus = document.querySelectorAll(".menus button");
-menus.forEach((menu) => menu.addEventListener("click",(event) => getNewsByTopic(event)));
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByTopic(event))
+);
 
-const getLatesNews = async () => {
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
-    let header = new Headers({"x-api-key": "rKMtkyXEHP4cODwLczm4TQKW4ohqBxbBq-313hdvipw",});
+let searchButton = document.getElementById("search-button");
+let url;
+
+const getNews =  async () => {
+    let header = new Headers({
+        "x-api-key": "rKMtkyXEHP4cODwLczm4TQKW4ohqBxbBq-313hdvipw",
+      });
+
+      let response = await fetch(url, { headers: header });
+      let data = await response.json();
+      news = data.articles;
+      console.log(news);
     
-    let response = await fetch(url, { headers: header});
-    let data = await response.json();
-    news = data.articles;
-    console.log(news);
-
-    render()
-};
-
-const getNewsByTopic = (event) => {
-
+      render();
 }
 
+const getLatesNews = async () => {
+  url = new URL(
+    `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`
+  );
+    getNews();
+};
+
+const getNewsByTopic = async (event) => {
+  let topic = event.target.textContent.toLowerCase();
+  url = new URL(
+    `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
+  );
+  getNews();
+};
+
+const getNewsByKeyword = async () => {
+  //1.검색 키워드 읽어오기
+  //2.url에 검색 키워드 부치기
+  //3.헤더준비
+  //4.url 부르기
+  //5.데이터 가져오기
+  //6.데이터 보여주기
+
+  let keyword = document.getElementById("search-input").value;
+  url = new URL(
+    `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
+  );
+    getNews();
+};
+
+const notImg =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU";
+
 const render = () => {
-    let newsHTML = ''   ;
-    newsHTML = news.map((item) => {
-        return `<div class="row news">
+  let newsHTML = "";
+  newsHTML = news
+    .map((item) => {
+      return `<div class="row news">
         <div class="col-lg-4">
-            <img class="news-img-size" src="${item.media}"/>
+      <img class="news-img-size" src="${item.media ? item.media : notImg}"/>  
         </div>
         <div class="col-lg-8">
             <h2>${item.title}</h2>
@@ -33,28 +69,33 @@ const render = () => {
             <div>
                 ${item.rights} ${item.published_date}
             </div>
+            <div>${news.rights || "no source"}  
+            ${moment(news.published_date).fromNow()}
+            </div>
         </div>
     </div>`;
-    }).join('');
+    })
+    .join("");
 
-    document.getElementById("news-board").innerHTML = newsHTML;
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
+searchButton.addEventListener("click", getNewsByKeyword);
 getLatesNews();
 
 const openSearchBox = () => {
-    let inputArea = document.getElementById("input-area");
-    if(inputArea.style.display === "inline") {
-        inputArea.style.display = "none";
-    } else {
-        inputArea.style.display = "inline";
-    }
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
 };
 
 const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 };
 
 const closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 };
